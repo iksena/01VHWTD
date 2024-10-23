1f4859efb3fa536a66671ee3546331e005111cd8
I Komang Sena Aji Buwana <v-i.sena@btpn.com>
2024-10-23 15:01:23 +0200

    add comments to lab 4

--- Lab4Ex1/336282_Lab04Ex01.py
+++ Lab4Ex1/336282_Lab04Ex01.py
@@ -1,9 +1,13 @@
-def wordsInPiecesExercise():
+# Author: S336282
+# Computer Programming in Python, A.A. 2024/25
+# Lab Practice 04, Part 1 Basic Cycles, 04.1.7 Words in pieces
+
+def wordsInPieces_s336282():
     word = input("Input word: ")
     
     for i in range(1,len(word)+1): # if word length is 3: i = 1 to 3
         for j in range(len(word)): # if word length is 3: j = 0 to 2
             if i+j <= len(word): # if word length is 3: i+j should not exceed 3
-                print(word[j:i+j])
+                print(word[j:i+j]) # if word length is 3: print 0:1, 1:2, 2:3, 0:2, 1:3, 0:3
 
-wordsInPiecesExercise()
+wordsInPieces_s336282()
